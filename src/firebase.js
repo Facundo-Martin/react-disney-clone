06346433367ab7f51391b65ebae4850a5278f4a4
Facundo Martin <facundomartin98@gmail.com>
2022-03-10 20:32:01 -0500

    pre-firebase commit

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAszNjvjbyDdzviuY0mqNQ22U3uV97xas8",
+  authDomain: "disney-clone-3835b.firebaseapp.com",
+  projectId: "disney-clone-3835b",
+  storageBucket: "disney-clone-3835b.appspot.com",
+  messagingSenderId: "199926631246",
+  appId: "1:199926631246:web:2d9067a6a248e52b4d2c1f",
+  measurementId: "G-4WY2C7CRGY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
